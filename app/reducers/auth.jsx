94d5affa042ce4d1757b501e57acadf5e6602890
Julius Cassin <kohwoojin@gmail.com>
2017-04-18 14:59:03 -0400

    Changes

--- app/reducers/auth.jsx
+++ app/reducers/auth.jsx
@@ -14,11 +14,13 @@ export const authenticated = user => ({
 })
 
 export const login = (username, password) =>
-  dispatch =>
-    axios.post('/api/auth/login/local',
+  dispatch => {
+    console.log('GOT TO LOGIN FUNCTION');
+    return axios.post('/api/auth/login/local',
       {username, password})
       .then(() => dispatch(whoami()))
       .catch(() => dispatch(whoami()))
+  }
 
 export const logout = () =>
   dispatch =>
--- app/routes.jsx
+++ app/routes.jsx
@@ -1,39 +1,44 @@
-import React from 'react'
-import { Router, Route, IndexRedirect, browserHistory } from 'react-router'
-import { connect } from 'react-redux'
+import React from 'react';
+import { Router, Route, IndexRedirect, browserHistory } from 'react-router';
+import { connect } from 'react-redux';
 
 /* CONTAINERS */
-import store from './store'
-import ExampleApp from './components/ExampleApp'
-import Jokes from './components/Jokes'
-import Login from './components/Login'
-import WhoAmI from './components/WhoAmI'
-import NotFound from './components/NotFound'
+import store from './store';
+import ExampleApp from './components/ExampleApp';
+import Jokes from './components/Jokes';
+import Login from './components/Login';
+import WhoAmI from './components/WhoAmI';
+import NotFound from './components/NotFound';
 
-import ProductCatalog from './product_catalog/containers'
+import ProductCatalog from './product_catalog/containers';
+import Signup from './signup/containers'
 
 /* Dispatchers */
-import { loadAllProducts } from './product_catalog/action-creators'
+import { loadAllProducts } from './product_catalog/action-creators';
 
 /* ROUTES COMPONENT */
 const RoutesComponent = ({ loadProductOnEnter }) => (
   <Router history={browserHistory}>
-    <Route path="/" component={ExampleApp}>
+    {/*<Route path="/" component={ExampleApp}>
       <IndexRedirect to="/jokes" />
       <Route path="/jokes" component={Jokes} />
+      <Route path="/products" component={ProductCatalog} onEnter={loadProductOnEnter} />
+    </Route>*/}
+    <Route path="/">
+      <Route path="/Signup" component={Signup} />
       <Route path="/products" component={ProductCatalog} onEnter={loadProductOnEnter} />
     </Route>
     <Route path="*" component={NotFound} />
   </Router>
-)
+);
 
 /* ROUTES CONTAINER */
 const mapStateToProps = state => ({})
 
 const mapDispatchToProps = dispatch => ({
   loadProductOnEnter: () => dispatch(loadAllProducts())
-})
+});
 
-const Routes = connect(mapStateToProps, mapDispatchToProps)(RoutesComponent)
+const Routes = connect(mapStateToProps, mapDispatchToProps)(RoutesComponent);
 
-export default Routes
+export default Routes;
