340c724512fc33bc609be6cbc352e8cb4a587b8a
JonathanLiu25 <JonathanLiu25@gmail.com>
2017-04-19 14:27:28 -0400

    changed style for navbar

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -65,28 +65,35 @@ const Navbar = props => {
 
           {/* Account Related Links */}
           <ul className="nav navbar-nav navbar-right">
+
+            {/* User greeting if they are logged in */}
             {user
-              ? <li className="dropdown">
-                <Link
-                  to="#"
-                  className="dropdown-toggle"
-                  data-toggle="dropdown"
-                  role="button"
-                  aria-haspopup="true"
-                  aria-expanded="false">
-                  Account <span className="caret" />
-                </Link>
-                <ul className="dropdown-menu">
-                  <li><Link to="/account">Your Account</Link></li>
-                  <li><Link to="#">Your Orders</Link></li>
-                  <li><Link to="#">Your Wishlist</Link></li>
-                  <li role="separator" className="divider" />
-                  <li><button onClick={logout}>Sign Out</button></li>
-                </ul>
-              </li>
-              : <li><Link to="/login">Login</Link></li>
+              ? <ul className="nav navbar-nav">
+                <li><Link>Hello, {user.email}!</Link></li>
+                <li className="dropdown">
+                  <Link
+                    to="#"
+                    className="dropdown-toggle"
+                    data-toggle="dropdown"
+                    role="button"
+                    aria-haspopup="true"
+                    aria-expanded="false">
+                    Account <span className="caret" />
+                  </Link>
+                  <ul className="dropdown-menu">
+                    <li><Link to="/account">Your Account</Link></li>
+                    <li><Link to="#">Your Orders</Link></li>
+                    <li><Link to="#">Your Wishlist</Link></li>
+                    <li role="separator" className="divider" />
+                    <li><Link to="#" onClick={logout}>Sign Out</Link></li>
+                  </ul>
+                </li>
+              </ul>
+              : <ul className="nav navbar-nav">
+                <li><Link to="/login">Login</Link></li>
+                <li><Link to="/signup">Sign Up</Link></li>
+              </ul>
             }
-            <li><Link to="/signup">Sign Up</Link></li>
             <li><Link to="#">Orders</Link></li>
             <li><Link to="#">Cart</Link></li>
           </ul>
@@ -104,9 +111,6 @@ const Navbar = props => {
               </div>
             </div>
           </form>
-
-          {/* User greeting if they are logged in */}
-          {user && <p>Hello {user.email}!</p>}
 
         </div>
       </div>
