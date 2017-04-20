f1aa0070bdc01f08c4bf605ed2dba2de986a0933
Julius Cassin <kohwoojin@gmail.com>
2017-04-20 12:26:39 -0400

    Added test for Orders and OrdersItems

--- db/models/order.test.js
+++ db/models/order.test.js
@@ -1,12 +1,12 @@
 'use strict'
 
 const db = require('APP/db')
-    , {Order} = db
+    , {Order, OrderItem} = db
     , {expect} = require('chai')
 
 /* global describe it before afterEach */
 
-describe('User', () => {
+describe('Order', () => {
   before('Await database sync', () => db.didSync)
   afterEach('Clear the tables', () => db.truncate({ cascade: true }))
 
@@ -17,5 +17,26 @@ describe('User', () => {
     it('Orders can be created with a paid status of true if set explicitly.', () =>
       Order.create({ paidStatus: true })
         .then(order => expect(order.paidStatus).to.be.true))
+    it('Orders can contain multiple order items', () =>
+      Order.create({
+        orderItems: [
+          {
+            deliveryStatus: 'waiting',
+            price: 1.00,
+            quantity: 3,
+          },
+          {
+            deliveryStatus: 'delivered',
+            price: 2.00,
+            quantity: 20,
+          }
+        ]
+      }, {
+        include: [ OrderItem ]
+      })
+        .then(user => user.getOrderItems())
+        .then(orderItems => {
+          expect(orderItems.length).to.be.equal(2)
+        }))
   })
 })
