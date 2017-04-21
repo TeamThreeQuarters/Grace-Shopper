0e3ead0b5c7f674b93aedb994e813ed6c71863ca
Samir Awuapara <samir8787@gmail.com>
2017-04-21 15:31:31 -0400

    findOrCreate Shopping Cart.

--- server/shoppingCart.js
+++ server/shoppingCart.js
@@ -20,23 +20,27 @@ module.exports = require('express').Router()
     console.log('/api/shoppingCart POST  sdgfsdgdf')
     let where = {}
     if (req.user) where = {user_id: req.user.id}
-    if (req.session.shoppingCartId) where =  {id: req.session.shoppingCartId}
+    if (req.session.shoppingCartId) where = {id: req.session.shoppingCartId}
 
     const defaults = req.user ? {user_id: req.user.id} : {}
     console.log('where', where)
     console.log('defaults', defaults)
-    ShoppingCart.create({user_id: 1})
-      .then(res => {
-        console.log('res', res)
-        // console.log(shoppingCart)
-        // req.session.shoppingCartId = shoppingCart.id
-        // shoppingCart.createShopping_cart_item({
-        //   quantity: req.body.quantity,
-        //   price: req.body.price
-        // })
+
+    ShoppingCart.findOrCreate({
+      where,
+      defaults,
+    })
+      .spread((shoppingCart) => {
+        console.log('shoppingCart', shoppingCart)
+        req.session.shoppingCartId = shoppingCart.id
+        shoppingCart.createShopping_cart_item({
+          quantity: req.body.quantity,
+          price: req.body.price
+        })
       })
       .catch((err) => {
         console.error('error', err)
+        next(err)
       })
       .then(res.sendStatus(201))
 
