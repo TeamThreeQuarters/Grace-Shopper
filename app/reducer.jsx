3cb38780f054d1b9e43b810873f224617598dbb0
Samir Awuapara <samir8787@gmail.com>
2017-04-21 17:07:50 -0400

    Create Shopping Cart constants and reducer.

--- app/reducer.jsx
+++ app/reducer.jsx
@@ -2,7 +2,8 @@ import { combineReducers } from 'redux'
 
 const rootReducer = combineReducers({
   auth: require('./reducers/auth').default,
-  products: require('./product_catalog/reducer').default
+  products: require('./product_catalog/reducer').default,
+  shoppingCart: require('./shopping_cart/reducer')
 })
 
 export default rootReducer
