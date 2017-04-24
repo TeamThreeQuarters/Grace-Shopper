97fe5311bd0c12bdce994195054a4c421fbd9ddd
Samir Awuapara <samir8787@gmail.com>
2017-04-24 18:33:31 -0400

    Style ProductDetail and add Add to Cart button.

--- app/product_catalog/components/ProductDetail.jsx
+++ app/product_catalog/components/ProductDetail.jsx
@@ -6,13 +6,16 @@ const ProductDetail = props => {
   return (
     <div className="container">
       {product.images &&
-        <div className="product-item">
+        <div className="product_detail">
           <div>
             <img src={product.images[0]} className="product-image" />
           </div>
-          <div>
+          <div className="product_info">
+            <span>Fullstack</span>
             <h3>{props.product.name}</h3>
+            <span>Price: $9.99</span>
             <p>{props.product.description}</p>
+            <button className="btn-primary">Add to Cart</button>
           </div>
         </div>
       }
