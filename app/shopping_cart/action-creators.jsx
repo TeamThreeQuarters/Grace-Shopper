0d3c5686b3cf40dde019416b78e20537f92ae57c
Samir Awuapara <samir8787@gmail.com>
2017-04-24 11:17:37 -0400

    Clean action creators and constants. Create thunk dispatchers.

--- app/shopping_cart/action-creators.jsx
+++ app/shopping_cart/action-creators.jsx
@@ -1,6 +1,8 @@
 'use strict'
 
-import { ADD_ITEM, REMOVE_ITEM } from './constants'
+import axios from 'axios'
+
+import { ADD_ITEM, UPDATE_QUANTITY,REMOVE_ITEM } from './constants'
 
 // ACTION CREATORS
 export const addItem = item => ({
@@ -8,7 +10,33 @@ export const addItem = item => ({
   item: item
 })
 
+export const updateQuantity = (itemId, quantity) => ({
+  type: UPDATE_QUANTITY,
+  itemId: itemId,
+  quantity: quantity
+})
+
 export const removeItem = itemId => ({
   type: REMOVE_ITEM,
   itemId: itemId
 })
+
+/* THUNK DISPATCHERS */
+
+export const addToShoppingCart = product => dispatch => {
+  axios.post('/api/shoppingCart', product)
+    .then((shoppingCartItem) => dispatch(addItem(shoppingCartItem)))
+    .catch(err => console.error('Error adding item to shopping cart', err))
+}
+
+export const updateShoppingCartItemQuantity = (shoppingCartItemId, quantity) => dispatch => {
+  axios.patch(`/api/shoppingCart/${shoppingCartItemId}`, {quantity: quantity})
+    .then(() => dispatch(updateQuantity(shoppingCartItemId, quantity)))
+    .catch(err => console.error('Error updating quantity', err))
+}
+
+export const removeFromShoppingCart = shoppingCartItem => dispatch => {
+  axios.delete(`/api/shoppingCart/${shoppingCartItem.id}`)
+    .then(() => dispatch(removeItem(shoppingCartItem.id)))
+    .catch(err => console.error('Error removing item from shopping cart', err))
+}
