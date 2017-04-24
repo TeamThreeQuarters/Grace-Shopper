6a58bf5a65414cad0fb4f9bd2f43789292f70da1
JonathanLiu25 <JonathanLiu25@gmail.com>
2017-04-24 14:38:57 -0400

    add test to products and single product page

--- app/product_catalog/components/index.jsx
+++ app/product_catalog/components/index.jsx
@@ -3,25 +3,29 @@
 import React from 'react'
 import { Link } from 'react-router'
 
-const ProductCatalog = props => (
-  <div className="container">
-    <div>
-      Products:
+const ProductCatalog = props => {
+  const products = props.products
+
+  return (
+    <div className="container">
+      <div>
+        Products:
     </div>
-    <div>
-      {props.products.map(product => (
-        <div key={product.id}>
-          <Link to={`/product/${product.name}/${product.id}`}>
-            <span>
-              <img src={product.images[0]} className="product-image" />
-            </span>
-            <span> {product.name} : {product.description} </span>
-          </Link>
-          <hr />
-        </div>
-      ))}
+      <div>
+        {products && products.map(product => (
+          <div className="product-item" key={product.id}>
+            <Link to={`/product/${product.name}/${product.id}`}>
+              <span>
+                <img src={product.images[0]} className="product-image" />
+              </span>
+              <span> {product.name} : {product.description} </span>
+            </Link>
+            <hr />
+          </div>
+        ))}
+      </div>
     </div>
-  </div>
-)
+  )
+}
 
 export default ProductCatalog
