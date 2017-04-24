507cf131a82ad982f1c06def6fc6ded3c9f9d55e
JonathanLiu25 <JonathanLiu25@gmail.com>
2017-04-24 16:39:42 -0400

    fix lint issues and sequelize warnings

--- db/seed/orders.js
+++ db/seed/orders.js
@@ -4,16 +4,14 @@ const db = require('APP/db')
 const { Order } = db
 const seed = require('./_seed')
 
-const orders = seed(Order, ({ users }) => {
-  return {
-    order1: {
-      paidStatus: true,
-      user_id: users.god.id
-    },
-    order2: {
-      paidStatus: false,
-      user_id: users.god.id
-    }
+const orders = seed(Order, ({ users }) => ({
+  order1: {
+    paidStatus: true,
+    user_id: users.god.id
+  },
+  order2: {
+    paidStatus: false,
+    user_id: users.god.id
   }
 }))
 
