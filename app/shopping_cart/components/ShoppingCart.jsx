63e53c88296915fb675c1f4d189dece44db97b31
Samir Awuapara <samirawuapara@Samirs-MacBook-Air.local>
2017-04-25 23:30:10 -0400

    Style ShoppingCart

--- app/shopping_cart/components/ShoppingCart.jsx
+++ app/shopping_cart/components/ShoppingCart.jsx
@@ -5,12 +5,16 @@ const ShoppingCart = props => {
   const checkout = props.checkout
   const message = props.message
   const cartCount = Object.keys(shoppingCartItems).length
-
+  console.log(shoppingCartItems);
   return (
-    <div className="container">
-      <h4>Hi, you have </h4>
-      {shoppingCartItems && <h4>{cartCount} items in your cart.</h4>}
-      <button onClick={checkout}>Checkout</button>
+    <div className="container shopping_cart">
+      <div style={{display: 'flex', justifyContent: 'space-between', alignItems: 'center'}}>
+        <h3 style={{marginTop: 10}}>Shopping Cart </h3>
+        <button style={{height: 30}} onClick={checkout}>Checkout</button>
+      </div>
+      <div style={{display: 'flex'}}>
+        {shoppingCartItems && <h4>{cartCount} items in your cart.</h4>}
+      </div>
       {message && <p>{message}</p>}
     </div>
   )
