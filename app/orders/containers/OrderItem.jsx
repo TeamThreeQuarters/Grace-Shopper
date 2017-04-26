56b40954610f7338bc99858c16d70ce00a99156d
JonathanLiu25 <JonathanLiu25@gmail.com>
2017-04-25 20:04:52 -0400

    add feature: user can add and change review/ratings

--- app/orders/containers/OrderItem.jsx
+++ app/orders/containers/OrderItem.jsx
@@ -2,6 +2,8 @@
 
 import React from 'react'
 import { connect } from 'react-redux'
+import { browserHistory } from 'react-router'
+import axios from 'axios'
 
 import OrderItem from '../components/OrderItem'
 
@@ -27,15 +29,34 @@ class OrderItemLocalContainer extends React.Component {
 
   reviewSubmitHandler(event) {
     event.preventDefault()
+    const productReview = this.props.orderItem.productReview
+
+    const review = {
+      rating: this.state.rating,
+      review: this.state.review,
+      order_item_id: this.props.orderItem.id,
+      product_id: this.props.product.id
+    }
+
+    if (productReview) {
+      const productReviewId = productReview.id
+      axios.put(`/api/productReview/${productReviewId}`, review)
+        .then(() => browserHistory.push(`/product/${this.props.product.name}/${this.props.product.id}`))
+        .catch(err => console.error('Could not submit review', err))
+    } else {
+      axios.post('/api/productReview', review)
+        .then(() => browserHistory.push(`/product/${this.props.product.name}/${this.props.product.id}`))
+        .catch(err => console.error('Could not submit review', err))
+    }
   }
 
   render() {
     return (
       <OrderItem
-      {...this.props}
-      ratingChangeHandler={this.ratingChangeHandler}
-      reviewChangeHandler={this.reviewChangeHandler}
-      reviewSubmitHandler={this.reviewSubmitHandler} />
+        {...this.props}
+        ratingChangeHandler={this.ratingChangeHandler}
+        reviewChangeHandler={this.reviewChangeHandler}
+        reviewSubmitHandler={this.reviewSubmitHandler} />
     )
   }
 }
